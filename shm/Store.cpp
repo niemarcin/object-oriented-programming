9837a7d7f564b9701c0375c0465ac3e543546e56
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-07-11 23:57:34 +0200

    cargo storage added in Store; nextDay method implemented; generateCargo implemented; randomization of cargo amount implemented; operator<< slighltly changed

--- shm/Store.cpp
+++ shm/Store.cpp
@@ -1,9 +1,20 @@
 #include "Store.hpp"
 
+#include "Alcohol.hpp"
+#include "Fruit.hpp"
+#include "Item.hpp"
+
 #include <algorithm>
+#include <iomanip>
+#include <random>
+
+Store::Store() {
+    generateCargo();
+}
 
 Store::Store(std::shared_ptr<Time>& time)
     : time_(time) {
+    generateCargo();
     time_->addObserver(this);
 }
 
@@ -17,17 +28,70 @@ Response Store::buy(Cargo* cargo, size_t amount, Player* player) {
         return Response::lack_of_space;
     }
     *cargo += amount;
-    player->purchaseCargo(std::shared_ptr<Cargo> (cargo), amount, price);
+    player->purchaseCargo(CargoPtr (cargo), amount, price);
     return Response::done;
 }
 
 Response Store::sell(Cargo* cargo, size_t amount, Player* player) {
     const size_t price = amount * cargo->getPrice();
     *cargo -= amount; 
-    player->sellCargo(std::shared_ptr<Cargo> (cargo), amount, price);
+    player->sellCargo(CargoPtr (cargo), amount, price);
     return Response::done;
 }
 
 void Store::nextDay() {
-    //Doing nothing for now. Interface purposes
+    randomizeCargoAmount();
+}
+
+void Store::generateCargo() {
+    cargo_.emplace_back(std::make_shared<Alcohol>("Rum", 150, 100, 60));
+    cargo_.emplace_back(std::make_shared<Alcohol>("Vodka", 20, 85, 70));
+    cargo_.emplace_back(std::make_shared<Fruit>("Apple", 10, 5));
+    cargo_.emplace_back(std::make_shared<Item>("Best item ever", 1, 1000, Rarity::legendary));
+}
+
+void Store::randomizeCargoAmount() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(0, 20);
+
+    std::for_each(cargo_.begin(), cargo_.end(), [this, &distrib, &gen](const auto& el){
+        *el += distrib(gen);
+    });
+}
+
+std::ostream& operator<<(std::ostream& out, const Store& store) {
+    std::string horizontalSeparator(61, '=');
+    int i = 0;
+    out << horizontalSeparator 
+              << "\n"
+              << "|| NAME"
+              << std::setw(27) << "| QTY "
+              << std::setw(5) << "| BUY PRICE "
+              << std::setw(3) << "| SELL PRICE"
+              << std::setw(3) << " ||\n"
+              << horizontalSeparator << "\n";
+
+    for (const CargoPtr& el : store.cargo_) {
+        out << "||"
+                  << std::setw(2) << ++i << ". "
+                  << std::setw(21) << std::left << el->getName() << " | "
+                  << std::setw(3) << std::right << el->getAmount() << " | "
+                  << std::setw(9) << std::right << store.calculateBuyPrice(el.get()) << " | "
+                  << std::setw(10) << std::right << el->getPrice() << " ||\n";
+    }
+    out << horizontalSeparator << "\n";
+    
+    return out;
+}
+
+size_t Store::calculateBuyPrice(Cargo* cargo) const {
+    const size_t cargoPriceThreshold = 10;
+    const double belowThreshMultiplier = 1.6;
+    const double aboveThreshMultiplier = 1.2;
+
+    size_t cargoPrice = cargo->getPrice();
+    double buyPriceMultiplier = (cargoPrice <= cargoPriceThreshold ? belowThreshMultiplier : aboveThreshMultiplier);
+
+    return cargoPrice * buyPriceMultiplier;
 }
--- shm/Store.hpp
+++ shm/Store.hpp
@@ -14,12 +14,21 @@ enum class Response {
 class Store : public Observer {
 private:
     std::shared_ptr<Time> time_{nullptr};
+    CargoPtrsVector cargo_;
+
+    void generateCargo();
+    void randomizeCargoAmount();
+    size_t calculateBuyPrice(Cargo* cargo) const;
 
 public:
+    Store();
     Store(std::shared_ptr<Time>& time);
+    Cargo* getCargo(const size_t pos) { return cargo_[pos].get(); }
     Response buy(Cargo* cargo, size_t amount, Player* player);
     Response sell(Cargo* cargo, size_t amount, Player* player);
 
     //Override from Observer
     void nextDay() override;
+
+    friend std::ostream& operator<<(std::ostream& out, const Store& store);
 };
